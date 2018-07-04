c7a5e6de3ef2f140cd635b846bce3e77b45f4a37
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-04 00:13:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,17 +16,17 @@ class Artist
     @songs = []
   end
 
-#  def self.all
-#    @@artists
-#  end
+  def self.all
+    @@artists
+  end
 #
 #  def self.reset_all
 #    self.all.clear
 #  end
 
-  def self.count
-    @@artists.count
-  end
+#  def self.count
+#    @@artists.count
+#  end
 
   def add_song(song)
     @songs << song
